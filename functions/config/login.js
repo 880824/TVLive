660d586d34d157810151266339917b7f3e96347c
880824 <mengzehe@gmail.com>
2026-07-17 08:01:46 +0800

    修复

--- functions/config/login.js
+++ functions/config/login.js
@@ -1,5 +1,5 @@
 // 路由：/config/login  →  登录提交
-import { handleLogin } from './_lib/handlers.js';
+import { handleLogin } from '../_lib/handlers.js';
 
 export async function onRequest(context) {
   return handleLogin(context.request, context.env);
